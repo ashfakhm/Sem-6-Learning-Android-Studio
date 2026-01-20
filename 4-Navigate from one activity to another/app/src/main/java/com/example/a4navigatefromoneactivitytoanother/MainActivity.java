ac7e9f10f66a7eacf3db2ec5970ed427861f31c1
Ashfakh M <editorashfakh@gmail.com>
2026-01-20 17:42:26 +0530

    Implemented Navigate from one activity to another program

---
+++ 4-Navigate from one activity to another/app/src/main/java/com/example/a4navigatefromoneactivitytoanother/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a4navigatefromoneactivitytoanother;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Next(View v){
+        Intent i = new Intent(this, SecondActivity.class);
+        startActivity(i);
+    }
+}
