c915e8dacf9659b382d1584b429ae3e4f72d1333
Ashfakh M <editorashfakh@gmail.com>
2026-01-22 05:53:21 +0530

    Implemented List Program

---
+++ 7ImplementList/app/src/main/java/com/example/a7implementlist/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.a7implementlist;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ListView lv = findViewById(R.id.l1);
+        String f1[] = {"Biriyani", "Mandhi", "FriedRice", "NormalRice"};
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, f1);
+        lv.setAdapter(ad);
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String s = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+}
